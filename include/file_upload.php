10934c31694ac451bf2a5d6fdc0bb3a92a1e65a2
Francesco Comi <francesco.comi@gmail.com>
2012-04-05 11:53:02 +0200

    Corrette le informazioni di licenza

--- include/file_upload.php
+++ include/file_upload.php
@@ -1,31 +1,13 @@
 <?php
 
 /*
- * ----------------------------------------------------------------------------
- * Decoro Urbano version 0.4.0
- * ----------------------------------------------------------------------------
- * Copyright Maiora Labs Srl (c) 2012
- * ----------------------------------------------------------------------------   
+ * http://github.com/valums/file-uploader
  * 
- * This file is part of Decoro Urbano.
+ * Multiple file upload component with progress-bar, drag-and-drop. 
+ * © 2010 Andrew Valums ( andrew(at)valums.com ) 
  * 
- * Decoro Urbano is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Decoro Urbano is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with Decoro Urbano.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
- * Insieme di classi per la gestione dell'upload dei file
- */
+ * Licensed under GNU GPL 2 or later, see license.txt.
+ */    
 
 /**
  * Handle file uploads via XMLHttpRequest
